cb5ae0a7424ecb88ca3d5e8a38fcb467128a3d8e
Miranda Thewlis <mkthewlis@gmail.com>
2020-10-08 13:01:29 +0200

    feat: began to add state to App class to prepare for button functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Bob', age: 28 },
+      { name: 'Cat', age: 29 },
+      { name: 'Sally', age: 20},
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1> 
-        <p>React, here I come!</p>
-        <Person name="Bob" age="28"/>
-        <Person name="Cat" age="29">My Hobbies: Racing</Person>
-        <Person name="Sally" age="20"/>
+        <h1>Hi, I'm a React App</h1> 
+        <p>Let's try this out!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
